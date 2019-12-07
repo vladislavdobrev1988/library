b3b198dd896fe5101b57ee2ded3bf45e606c637d
Vladislav Dobrev <vladislavdobrev1988@gmail.com>
2019-12-07 18:27:02 +0200

    minor improvement

--- Library/OnStartup/Logging.cs
+++ Library/OnStartup/Logging.cs
@@ -4,13 +4,11 @@ namespace Library.OnStartup
 {
     public class Logging
     {
-        private const string DATE_FORMAT = "yyyy-MM";
-        private const string LOG_FILE_EXTENSION = ".txt";
+        private const string FORMAT = "{0:yyyy-MM}.txt";
 
         public static string GetLogFileName()
         {
-            var name = DateTime.UtcNow.ToString(DATE_FORMAT);
-            return string.Concat(name, LOG_FILE_EXTENSION);
+            return string.Format(FORMAT, DateTime.UtcNow);
         }
     }
 }
