36212b5a51eacd7fe8cc40b3724b5b3eae64cd1a
Vladislav Dobrev <vladislavdobrev1988@gmail.com>
2019-12-27 18:55:05 +0200

    SeedData moved to separate class

---
+++ Library.Objects/Context/SeedData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Library.Objects.Entities;
+
+namespace Library.Objects.Context
+{
+    public static class SeedData
+    {
+        public static Author[] Get()
+        {
+            return new Author[]
+            {
+                new Author
+                {
+                    FirstName = "John",
+                    LastName = "Steinbeck",
+                    DateOfBirth = DateTime.Parse("1902-02-27"),
+                    DateOfDeath = DateTime.Parse("1968-12-20"),
+                    NobelPrize = true,
+                    Books = new List<Book>
+                    {
+                        new Book
+                        {
+                            Title = "The Grapes of Wrath",
+                            ReleaseDate = DateTime.Parse("1939-04-14")
+                        },
+                        new Book
+                        {
+                            Title = "East of Eden",
+                            ReleaseDate = DateTime.Parse("1952-09-19")
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
--- Library/OnStartup/Database.cs
+++ Library/OnStartup/Database.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Library.Objects.Context;
-using Library.Objects.Entities;
+﻿using Library.Objects.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -17,38 +14,10 @@ namespace Library.OnStartup
 
                 if (context.Database.EnsureCreated())
                 {
-                    context.Authors.AddRange(GetSeedData());
+                    context.Authors.AddRange(SeedData.Get());
                     context.SaveChanges();
                 }
             }
-        }
-
-        private static Author[] GetSeedData()
-        {
-            return new Author[]
-            {
-                new Author
-                {
-                    FirstName = "John",
-                    LastName = "Steinbeck",
-                    DateOfBirth = DateTime.Parse("1902-02-27"),
-                    DateOfDeath = DateTime.Parse("1968-12-20"),
-                    NobelPrize = true,
-                    Books = new List<Book>
-                    {
-                        new Book
-                        {
-                            Title = "The Grapes of Wrath",
-                            ReleaseDate = DateTime.Parse("1939-04-14")
-                        },
-                        new Book
-                        {
-                            Title = "East of Eden",
-                            ReleaseDate = DateTime.Parse("1952-09-19")
-                        }
-                    }
-                }
-            };
         }
     }
 }
