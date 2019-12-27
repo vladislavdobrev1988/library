1fad4d333e21622b7f91ca019253623badbb424e
vladislav.dobrev <vladislav.dobrev@ezbob.com>
2019-12-27 17:34:44 +0200

    seed functionality started

--- Library/OnStartup/Database.cs
+++ Library/OnStartup/Database.cs
@@ -1,4 +1,7 @@
-﻿using Library.Objects.Context;
+﻿using System;
+using System.Collections.Generic;
+using Library.Objects.Context;
+using Library.Objects.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,8 +15,40 @@ namespace Library.OnStartup
             {
                 var context = scope.ServiceProvider.GetService<LibraryContext>();
 
-                context.Database.EnsureCreated();
+                if (context.Database.EnsureCreated())
+                {
+                    context.Authors.AddRange(GetSeedData());
+                    context.SaveChanges();
+                }
             }
+        }
+
+        private static Author[] GetSeedData()
+        {
+            return new Author[]
+            {
+                new Author
+                {
+                    FirstName = "John",
+                    LastName = "Steinbeck",
+                    DateOfBirth = DateTime.Parse("1902-02-27"),
+                    DateOfDeath = DateTime.Parse("1968-12-20"),
+                    NobelPrize = true,
+                    Books = new List<Book>
+                    {
+                        new Book
+                        {
+                            Title = "The Grapes of Wrath",
+                            ReleaseDate = DateTime.Parse("1939-04-14")
+                        },
+                        new Book
+                        {
+                            Title = "East of Eden",
+                            ReleaseDate = DateTime.Parse("1952-09-19")
+                        }
+                    }
+                }
+            };
         }
     }
 }
