9286ae2d6f404efa5b23f8c56cc0384d5e8beeb7
Vladislav Dobrev <vladislavdobrev1988@gmail.com>
2019-12-28 21:47:49 +0200

    some author model tests added

--- Library.Tests/ModelTests/AuthorModelTests.cs
+++ Library.Tests/ModelTests/AuthorModelTests.cs
@@ -1,7 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Library.Objects.Entities;
 using Library.Objects.Exceptions;
 using Library.Objects.Helpers.Constants;
 using Library.Objects.Models.Implementations;
+using Library.Objects.Proxies;
 using Library.Objects.Repositories.Interfaces;
 using Library.Objects.Validation.Interfaces;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -39,79 +43,222 @@ namespace Library.Tests.ModelTests
         [TestMethod]
         public async Task CreateAuthorAsync_NullFirstName_ThrowsException()
         {
+            var author = GetAuthor();
 
+            author.FirstName = null;
+
+            var ex = await Assert.ThrowsExceptionAsync<HttpResponseException>(async () => await _model.CreateAuthorAsync(author));
+
+            Assert.AreEqual(AuthorModel.ErrorMessage.FIRST_NAME_REQUIRED, ex.Message);
+            Assert.AreEqual(HttpStatusCode.BAD_REQUEST, ex.StatusCode);
         }
 
         [TestMethod]
         public async Task CreateAuthorAsync_EmptyFirstName_ThrowsException()
         {
+            var author = GetAuthor();
 
+            author.FirstName = " ";
+
+            var ex = await Assert.ThrowsExceptionAsync<HttpResponseException>(async () => await _model.CreateAuthorAsync(author));
+
+            Assert.AreEqual(AuthorModel.ErrorMessage.FIRST_NAME_REQUIRED, ex.Message);
+            Assert.AreEqual(HttpStatusCode.BAD_REQUEST, ex.StatusCode);
         }
 
         [TestMethod]
         public async Task CreateAuthorAsync_NullLastName_ThrowsException()
         {
+            var author = GetAuthor();
 
+            author.LastName = null;
+
+            var ex = await Assert.ThrowsExceptionAsync<HttpResponseException>(async () => await _model.CreateAuthorAsync(author));
+
+            Assert.AreEqual(AuthorModel.ErrorMessage.LAST_NAME_REQUIRED, ex.Message);
+            Assert.AreEqual(HttpStatusCode.BAD_REQUEST, ex.StatusCode);
         }
 
         [TestMethod]
         public async Task CreateAuthorAsync_EmptyLastName_ThrowsException()
         {
+            var author = GetAuthor();
 
+            author.LastName = " ";
+
+            var ex = await Assert.ThrowsExceptionAsync<HttpResponseException>(async () => await _model.CreateAuthorAsync(author));
+
+            Assert.AreEqual(AuthorModel.ErrorMessage.LAST_NAME_REQUIRED, ex.Message);
+            Assert.AreEqual(HttpStatusCode.BAD_REQUEST, ex.StatusCode);
         }
 
         [TestMethod]
         public async Task CreateAuthorAsync_NullNobelPrize_ThrowsException()
         {
+            var author = GetAuthor();
 
+            author.NobelPrize = null;
+
+            var ex = await Assert.ThrowsExceptionAsync<HttpResponseException>(async () => await _model.CreateAuthorAsync(author));
+
+            Assert.AreEqual(AuthorModel.ErrorMessage.NOBEL_PRIZE_REQUIRED, ex.Message);
+            Assert.AreEqual(HttpStatusCode.BAD_REQUEST, ex.StatusCode);
         }
 
         [TestMethod]
         public async Task CreateAuthorAsync_InvalidDateOfBirth_ThrowsException()
         {
+            var author = GetAuthor();
 
+            var dateOfBirthErrorMessage = "some date error";
+
+            _dateValidatorMock
+                .Setup(x => x.Validate(author.DateOfBirth))
+                .Returns(dateOfBirthErrorMessage);
+
+            var ex = await Assert.ThrowsExceptionAsync<HttpResponseException>(async () => await _model.CreateAuthorAsync(author));
+
+            Assert.AreEqual(dateOfBirthErrorMessage, ex.Message);
+            Assert.AreEqual(HttpStatusCode.BAD_REQUEST, ex.StatusCode);
         }
 
         [TestMethod]
         public async Task CreateAuthorAsync_InvalidDateOfDeath_ThrowsException()
         {
+            var author = GetAuthor();
 
+            var dateOfDeathErrorMessage = "some date error";
+
+            _dateValidatorMock
+                .Setup(x => x.Validate(author.DateOfDeath))
+                .Returns(dateOfDeathErrorMessage);
+
+            var ex = await Assert.ThrowsExceptionAsync<HttpResponseException>(async () => await _model.CreateAuthorAsync(author));
+
+            Assert.AreEqual(dateOfDeathErrorMessage, ex.Message);
+            Assert.AreEqual(HttpStatusCode.BAD_REQUEST, ex.StatusCode);
         }
 
         [TestMethod]
         public async Task CreateAuthorAsync_AlreadyExists_ThrowsException()
         {
+            var author = GetAuthor();
 
+            _repositoryMock
+                .Setup(x => x.GetByNameAsync(author.FirstName, author.LastName))
+                .Returns(Task.FromResult(new Author { Id = 1 }));
+
+            var ex = await Assert.ThrowsExceptionAsync<HttpResponseException>(async () => await _model.CreateAuthorAsync(author));
+
+            Assert.AreEqual(AuthorModel.ErrorMessage.AUTHOR_EXISTS, ex.Message);
+            Assert.AreEqual(HttpStatusCode.CONFLICT, ex.StatusCode);
         }
 
         [TestMethod]
         public async Task CreateAuthorAsync_ValidInput_WorksAsExpected()
         {
+            const int ID = 5;
 
+            var author = GetAuthor();
+
+            Expression<Func<Author, bool>> expected = a =>
+                a.FirstName == author.FirstName &&
+                a.LastName == author.LastName &&
+                a.NobelPrize == author.NobelPrize &&
+                a.DateOfBirth == DateTime.Parse(author.DateOfBirth) &&
+                a.DateOfDeath == DateTime.Parse(author.DateOfDeath);
+
+            _repositoryMock
+                .Setup(x => x.AddAsync(It.Is(expected)))
+                .Returns(Task.FromResult(ID));
+
+            var response = await _model.CreateAuthorAsync(author);
+
+            Assert.IsNotNull(response);
+            Assert.AreEqual(ID, response.Id);
         }
         
         [TestMethod]
         public async Task UpdateAuthorAsync_NullAuthor_ThrowsException()
         {
+            var ex = await Assert.ThrowsExceptionAsync<HttpResponseException>(async () => await _model.UpdateAuthorAsync(1, null));
 
+            Assert.AreEqual(AuthorModel.ErrorMessage.AUTHOR_REQUIRED, ex.Message);
+            Assert.AreEqual(HttpStatusCode.BAD_REQUEST, ex.StatusCode);
         }
 
         [TestMethod]
         public async Task UpdateAuthorAsync_AuthorNotFound_ThrowsException()
         {
+            const int UPDATED_AUTHOR_ID = 5;
 
+            var author = GetAuthor();
+
+            var ex = await Assert.ThrowsExceptionAsync<HttpResponseException>(async () => await _model.UpdateAuthorAsync(UPDATED_AUTHOR_ID, author));
+
+            var expectedMessage = string.Format(AuthorModel.ErrorMessage.NOT_FOUND_FORMAT, UPDATED_AUTHOR_ID);
+
+            Assert.AreEqual(expectedMessage, ex.Message);
+            Assert.AreEqual(HttpStatusCode.NOT_FOUND, ex.StatusCode);
         }
 
         [TestMethod]
         public async Task UpdateAuthorAsync_AlreadyExists_ThrowsException()
         {
+            const int UPDATED_AUTHOR_ID = 5;
+            const int DUPLICATE_AUTHOR_ID = 6;
 
+            var author = GetAuthor();
+
+            _repositoryMock
+                .Setup(x => x.GetByIdAsync(UPDATED_AUTHOR_ID))
+                .Returns(Task.FromResult(new Author { Id = UPDATED_AUTHOR_ID }));
+
+            _repositoryMock
+                .Setup(x => x.GetByNameAsync(author.FirstName, author.LastName))
+                .Returns(Task.FromResult(new Author { Id = DUPLICATE_AUTHOR_ID }));
+
+            var ex = await Assert.ThrowsExceptionAsync<HttpResponseException>(async () => await _model.UpdateAuthorAsync(UPDATED_AUTHOR_ID, author));
+
+            Assert.AreEqual(AuthorModel.ErrorMessage.AUTHOR_EXISTS, ex.Message);
+            Assert.AreEqual(HttpStatusCode.CONFLICT, ex.StatusCode);
         }
 
         [TestMethod]
         public async Task UpdateAuthorAsync_ValidInput_WorksAsExpected()
         {
+            const int UPDATED_AUTHOR_ID = 5;
 
+            var author = GetAuthor();
+            author.DateOfDeath = null;
+
+            var entity = new Author { Id = UPDATED_AUTHOR_ID };
+
+            _repositoryMock
+                .Setup(x => x.GetByIdAsync(UPDATED_AUTHOR_ID))
+                .Returns(Task.FromResult(entity));
+
+            await _model.UpdateAuthorAsync(UPDATED_AUTHOR_ID, author);
+
+            Assert.AreEqual(author.FirstName, entity.FirstName);
+            Assert.AreEqual(author.LastName, entity.LastName);
+            Assert.AreEqual(author.NobelPrize, entity.NobelPrize);
+            Assert.AreEqual(DateTime.Parse(author.DateOfBirth), entity.DateOfBirth);
+            Assert.IsNull(entity.DateOfDeath);
+
+            _repositoryMock.Verify(x => x.SaveChangesAsync(), Times.Once);
+        }
+
+        private AuthorProxy GetAuthor()
+        {
+            return new AuthorProxy
+            {
+                FirstName = "William",
+                LastName = "Shakespeare",
+                DateOfBirth = "1564-04-26",
+                DateOfDeath = "1616-04-23",
+                NobelPrize = false
+            };
         }
     }
 }
